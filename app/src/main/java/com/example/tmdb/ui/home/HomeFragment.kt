a36ca7e0c7f2c05939ae4a174931d26ca6a6c36c
Luis Scripcaru <luys50734@gmail.com>
2022-04-11 17:20:20 +0300

    migrated to dataBinding/viewBinding

--- app/src/main/java/com/example/tmdb/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/tmdb/ui/home/HomeFragment.kt
@@ -1,13 +1,13 @@
 package com.example.tmdb.ui.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tmdb.R
@@ -19,38 +19,34 @@ class HomeFragment : Fragment() {
     private lateinit var popularMovies: RecyclerView
     private lateinit var popularMoviesAdapter: MoviesAdapter
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding
-        get() = _binding!!
-
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+    private val binding get() = _binding!!
+    override fun onCr±eateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
-        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        val homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-
-        val root: View = binding.root
-
         popularMovies = binding.popularMovies
-        popularMovies.layoutManager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false)
+        popularMovies.layoutManager = LinearLayoutManager(context,LinearLayoutManager.VERTICAL,false)
         popularMoviesAdapter = MoviesAdapter(listOf())
+        popularMovies.addItemDecoration(DividerItemDecoration(context,DividerItemDecoration.VERTICAL))
         popularMovies.adapter = popularMoviesAdapter
-        MoviesRepository.getPopularMovies(onSuccess = ::onPopularMoviesFetched,onError =  ::onError)
 
-        return root
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        MoviesRepository.getPopularMovies(page = 1,onSuccess = ::onPopularMoviesFetched,onError =  ::onError)
     }
     private fun onPopularMoviesFetched(movies: List<Movie>) {
         popularMoviesAdapter.updateMovies(movies)
     }
-
     private fun onError() {
-       // Toast.makeText(this, getString(R.string.error_fetch_movies), Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, getString(R.string.error_fetch_movies), Toast.LENGTH_SHORT).show()
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
