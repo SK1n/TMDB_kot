b195dc22a9113c75953438295f4cacb456b8f24d
Luis Scripcaru <luys50734@gmail.com>
2022-05-10 21:53:39 +0300

    added LoadingState to tAdapter

--- app/src/main/java/com/example/tmdb/ui/tvShows/popularTv/PopularTvFragment.kt
+++ app/src/main/java/com/example/tmdb/ui/tvShows/popularTv/PopularTvFragment.kt
@@ -1,22 +1,18 @@
 package com.example.tmdb.ui.tvShows.popularTv
 
+
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AbsListView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.tmdb.adapters.TvShowsAdapter
 import com.example.tmdb.databinding.FragmentTvShowsPopularBinding
-import com.example.tmdb.utils.Constants.Companion.QUERY_PAGE_SIZE
-import com.example.tmdb.utils.Resource
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
@@ -40,11 +36,19 @@ class PopularTvFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        lifecycleScope.launch { viewModel.getData().collectLatest { tAdapter.submitData(it) } }
+        viewLifecycleOwner.lifecycleScope.launch {
+            tAdapter.loadStateFlow.collectLatest { loadStates ->
+                viewModel.isLoading.value = loadStates.refresh is LoadState.Loading
+            }
+        }
+        lifecycleScope.launch {
+            viewModel.getData().collectLatest { tAdapter.submitData(it) }
+        }
     }
 
     private fun setupRecyclerView() {
         tAdapter = TvShowsAdapter()
+
         binding.popularTvRecycler.apply {
             adapter = tAdapter
             layoutManager = LinearLayoutManager(activity)
--- app/src/main/java/com/example/tmdb/ui/tvShows/topRatedTv/TopRatedTvFragment.kt
+++ app/src/main/java/com/example/tmdb/ui/tvShows/topRatedTv/TopRatedTvFragment.kt
@@ -1,22 +1,17 @@
 package com.example.tmdb.ui.tvShows.topRatedTv
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AbsListView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.tmdb.adapters.TvShowsAdapter
 import com.example.tmdb.databinding.FragmentTvShowsTopRatedBinding
-import com.example.tmdb.utils.Constants
-import com.example.tmdb.utils.Resource
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
@@ -40,7 +35,14 @@ class TopRatedTvFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        lifecycleScope.launch { viewModel.getData().collectLatest { tAdapter.submitData(it) } }
+        viewLifecycleOwner.lifecycleScope.launch {
+            tAdapter.loadStateFlow.collectLatest { loadStates ->
+                viewModel.isLoading.value = loadStates.refresh is LoadState.Loading
+            }
+        }
+        lifecycleScope.launch {
+            viewModel.getData().collectLatest { tAdapter.submitData(it) }
+        }
     }
 
     private fun setupRecyclerView() {
